107f625e2b35894db00318318d30322a4fb40df3
Roni Jaakkola <roni.jaakkola@gmail.com>
2026-06-05 18:19:52 +0300

    add home screen

---
+++ src/c/home_window.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define HOME_HL_COLOR GColorLightGray
+
+void home_window_create();
+void home_window_destroy();
+Window *home_window_get_window();
--- src/c/main.c
+++ src/c/main.c
@@ -1,5 +1,6 @@
 #include <pebble.h>
 #include "splash_window.h"
+#include "home_window.h"
 #include "main_window.h"
 #include "lines_window.h"
 #include "error_window.h"
@@ -15,18 +16,19 @@ static void app_message_dropped(AppMessageResult reason, void *context) {
 	APP_LOG(APP_LOG_LEVEL_INFO, "Message dropped, reason: %d", reason);
 }
 
-void open_main_menu(void *data) {
+void open_home_screen(void *data) {
 	// Remove splash window from the stack because we don't want to come back to it
 	window_stack_remove(splash_window_get_window(), true);
 	// Free the splash window to save memory
 	splash_window_destroy();
-	window_stack_push(main_window_get_window(), true);
+	window_stack_push(home_window_get_window(), true);
 }
 
 void init()
 {
 	// Create all windows
 	splash_window_create();
+	home_window_create();
 	main_window_create();
 	lines_window_create();
 	error_window_create();
@@ -40,11 +42,12 @@ void init()
 	window_stack_push(splash_window_get_window(), true);
 
 	// Timeout to show splash screen only for a limited time
-	app_timer_register(1500, open_main_menu, NULL);
+	app_timer_register(1500, open_home_screen, NULL);
 }
 
 void deinit()
 {
+	home_window_destroy();
 	main_window_destroy();
 	lines_window_destroy();
 	error_window_destroy();
